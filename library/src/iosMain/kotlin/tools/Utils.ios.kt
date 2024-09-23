c008faa53984732c6a8c2463f7aaaa7718c78d51
Erick Steven Velasco Amaya <erickvelasco11@gmail.com>
2024-09-23 17:47:24 -0500

    Fix getCurrentLanguage iOS

--- library/src/iosMain/kotlin/tools/Utils.ios.kt
+++ library/src/iosMain/kotlin/tools/Utils.ios.kt
@@ -1,6 +1,8 @@
 package tools
 
+import platform.Foundation.NSLocale
 import platform.Foundation.currentLocale
+import platform.Foundation.languageCode
 
 actual fun getCurrentLanguage(): String {
     return NSLocale.currentLocale.languageCode ?: "en"
