ce0b7b9e8f836383e5ed071f7f63956f65731422
Erick Velasco <erickvelasco11@gmail.com>
2024-09-23 17:48:42 -0500

    Added SetStatusBarColor basic iOS implementation

--- library/src/iosMain/kotlin/tools/Visual.ios.kt
+++ library/src/iosMain/kotlin/tools/Visual.ios.kt
@@ -5,4 +5,29 @@ import androidx.compose.ui.graphics.Color
 
 @Composable
 actual fun SetStatusBarColor(color: Color, darkIcons: Boolean) {
+    val uiColor = UIColor(color)
+
+    if (UIApplication.sharedApplication.keyWindow != null) {
+        val statusBar =
+            UIView(frame = UIApplication.sharedApplication.keyWindow?.windowScene?.statusBarManager!!.statusBarFrame)
+        statusBar.backgroundColor = uiColor
+        UIApplication.sharedApplication.keyWindow!!.addSubview(statusBar)
+
+        val style = if (darkIcons) {
+            UIStatusBarStyle.MIN_VALUE
+        } else {
+            UIStatusBarStyle.MAX_VALUE
+        }
+
+        UIApplication.sharedApplication.setStatusBarStyle(style)
+    }
+}
+
+fun UIColor(color: Color): UIColor {
+    return UIColor(
+        red = color.red.toDouble(),
+        green = color.green.toDouble(),
+        blue = color.blue.toDouble(),
+        alpha = color.alpha.toDouble()
+    )
 }
