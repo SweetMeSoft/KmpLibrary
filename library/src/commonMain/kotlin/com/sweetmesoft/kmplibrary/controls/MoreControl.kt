06b4283faee103059ec4f7132ea2aaca7f1a4ee8
Erick Velasco <erickvelasco11@gmail.com>
2024-11-26 19:21:55 -0500

    Added icon to MoreControl

--- library/src/commonMain/kotlin/com/sweetmesoft/kmplibrary/controls/MoreControl.kt
+++ library/src/commonMain/kotlin/com/sweetmesoft/kmplibrary/controls/MoreControl.kt
@@ -1,7 +1,10 @@
 package com.sweetmesoft.kmplibrary.controls
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
@@ -16,7 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun MoreControl(modifier: Modifier = Modifier, options: List<ItemOption>) {
@@ -43,7 +48,17 @@ fun MoreControl(modifier: Modifier = Modifier, options: List<ItemOption>) {
                     item.onClick()
                     showMenu = false
                 }) {
-                    Text(item.text)
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        if (item.icon != null) {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = "Option",
+                                modifier = Modifier.size(20.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
+                        Text(item.text)
+                    }
                 }
             }
         }
@@ -52,5 +67,6 @@ fun MoreControl(modifier: Modifier = Modifier, options: List<ItemOption>) {
 
 data class ItemOption(
     val text: String,
+    val icon: ImageVector? = null,
     val onClick: () -> Unit
 )
