0dc581841787e553166ec40045d2a49fa58b1830
Erick Velasco <erickvelasco11@gmail.com>
2024-12-31 17:55:11 -0500

    Added new DatePicker Calendar

--- library/src/commonMain/kotlin/com/sweetmesoft/kmplibrary/controls/pickers/DatePicker.kt
+++ library/src/commonMain/kotlin/com/sweetmesoft/kmplibrary/controls/pickers/DatePicker.kt
@@ -2,30 +2,64 @@ package com.sweetmesoft.kmplibrary.controls.pickers
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sweetmesoft.kmplibrary.tools.DateFormats
+import com.sweetmesoft.kmplibrary.tools.daysInMonth
+import com.sweetmesoft.kmplibrary.tools.disabledColorDark
+import com.sweetmesoft.kmplibrary.tools.disabledColorText
+import com.sweetmesoft.kmplibrary.tools.disabledColorTextDark
 import com.sweetmesoft.kmplibrary.tools.toLocalString
+import compose.icons.TablerIcons
+import compose.icons.tablericons.ChevronLeft
+import compose.icons.tablericons.ChevronRight
+import kmp_library.library.generated.resources.Accept
+import kmp_library.library.generated.resources.Cancel
 import kmp_library.library.generated.resources.Date
+import kmp_library.library.generated.resources.FridayOneLetter
+import kmp_library.library.generated.resources.MondayOneLetter
 import kmp_library.library.generated.resources.Res
+import kmp_library.library.generated.resources.SaturdayOneLetter
+import kmp_library.library.generated.resources.SundayOneLetter
+import kmp_library.library.generated.resources.ThursdayOneLetter
+import kmp_library.library.generated.resources.TuesdayOneLetter
+import kmp_library.library.generated.resources.WednesdayOneLetter
+import kotlinx.datetime.DateTimeUnit.Companion.MONTH
 import kotlinx.datetime.LocalDate
-import network.chaintech.kmp_date_time_picker.ui.datepicker.WheelDatePickerView
-import network.chaintech.kmp_date_time_picker.utils.DateTimePickerView
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
 import network.chaintech.kmp_date_time_picker.utils.MAX
 import network.chaintech.kmp_date_time_picker.utils.MIN
 import org.jetbrains.compose.resources.stringResource
@@ -39,7 +73,7 @@ fun DatePicker(
     title: String = stringResource(Res.string.Date),
     minDate: LocalDate = LocalDate.MIN(),
     maxDate: LocalDate = LocalDate.MAX(),
-    selectDatePicker: (LocalDate) -> Unit
+    onSelectedDate: (LocalDate) -> Unit
 ) {
     var showPicker: Boolean by remember { mutableStateOf(false) }
     Box(modifier = modifier) {
@@ -74,37 +108,202 @@ fun DatePicker(
         )
     }
 
-    WheelDatePickerView(
-        modifier = Modifier.padding(top = 18.dp, bottom = 10.dp).fillMaxWidth()
-            .background(MaterialTheme.colors.surface),
-        showDatePicker = showPicker,
-        containerColor = MaterialTheme.colors.surface,
-        dateTextColor = MaterialTheme.colors.onSurface,
-        titleStyle = TextStyle(
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colors.onSurface
-        ),
-        doneLabelStyle = TextStyle(
-            fontSize = 16.sp,
-            fontWeight = FontWeight(600),
-            color = color,
-        ),
-        rowCount = 5,
+    CalendarDatePicker(
+        value = value,
+        color = color,
         minDate = minDate,
         maxDate = maxDate,
-        dateTextStyle = TextStyle(
-            fontWeight = FontWeight(600),
-            color = MaterialTheme.colors.onSurface
-        ),
-        dateTimePickerView = DateTimePickerView.DIALOG_VIEW,
-        onDoneClick = {
-            selectDatePicker(it)
+        acceptText = stringResource(Res.string.Accept),
+        cancelText = stringResource(Res.string.Cancel),
+        onDateSelected = { selectedDate ->
+            onSelectedDate(selectedDate)
             showPicker = false
         },
-        onDismiss = {
-            showPicker = false
-        },
-        height = 170.dp
+        onDismiss = { showPicker = false }
     )
+}
+
+@Composable
+private fun CalendarDatePicker(
+    value: LocalDate,
+    color: Color,
+    acceptText: String,
+    cancelText: String,
+    minDate: LocalDate,
+    maxDate: LocalDate,
+    onDateSelected: (LocalDate) -> Unit,
+    onDismiss: () -> Unit
+) {
+    var selectedDate by remember { mutableStateOf(value) }
+    var dateShown by remember { mutableStateOf(value) }
+    var selectYear by remember { mutableStateOf(false) }
+    val firstDayOfMonth = remember(dateShown.year, dateShown.monthNumber) {
+        LocalDate(dateShown.year, dateShown.monthNumber, 1).dayOfWeek.ordinal % 7
+    }
+    val scrollState = rememberLazyGridState(dateShown.year - 1900)
+    val daysOneLetter = listOf(
+        stringResource(Res.string.MondayOneLetter),
+        stringResource(Res.string.TuesdayOneLetter),
+        stringResource(Res.string.WednesdayOneLetter),
+        stringResource(Res.string.ThursdayOneLetter),
+        stringResource(Res.string.FridayOneLetter),
+        stringResource(Res.string.SaturdayOneLetter),
+        stringResource(Res.string.SundayOneLetter)
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.surface, RoundedCornerShape(8.dp))
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().background(
+                color = color,
+                shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp)
+            ).padding(16.dp)
+        ) {
+            Text(
+                modifier = Modifier.clickable {
+                    if (!selectYear) {
+                        selectYear = true
+                    }
+                },
+                text = selectedDate.year.toString(),
+                color = MaterialTheme.colors.onSurface
+            )
+            Text(
+                text = selectedDate.toLocalString(DateFormats.WWW_MMM_DD),
+                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colors.onSurface
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            IconButton(onClick = {
+                dateShown = dateShown.minus(1, MONTH)
+            }) {
+                Icon(TablerIcons.ChevronLeft, contentDescription = "Mes anterior")
+            }
+            Text(
+                text = dateShown.month.name + ", " + dateShown.year.toString(),
+                style = MaterialTheme.typography.subtitle1,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+            IconButton(onClick = {
+                dateShown = dateShown.plus(1, MONTH)
+            }) {
+                Icon(TablerIcons.ChevronRight, contentDescription = "Mes siguiente")
+            }
+        }
+
+        if (selectYear) {
+            LazyVerticalGrid(
+                state = scrollState,
+                modifier = Modifier.padding(horizontal = 8.dp).height(290.dp),
+                columns = GridCells.Fixed(3),
+            ) {
+                items(300) { year ->
+                    val enabled = (year + 1900) >= minDate.year && (year + 1900) <= maxDate.year
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                if (year == dateShown.year) color else MaterialTheme.colors.surface,
+                            )
+                            .clickable(enabled = enabled) {
+                                dateShown = LocalDate(
+                                    year + 1900,
+                                    selectedDate.monthNumber,
+                                    selectedDate.dayOfMonth
+                                )
+                                selectYear = false
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(vertical = 16.dp),
+                            text = (year + 1900).toString(),
+                            style = MaterialTheme.typography.body2,
+                            color = if ((year + 1900) == dateShown.year) color else MaterialTheme.colors.onSurface
+                        )
+                    }
+                }
+            }
+        } else {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(horizontal = 8.dp).height(290.dp),
+                columns = GridCells.Fixed(7),
+            ) {
+                itemsIndexed(daysOneLetter) { index, day ->
+                    Text(
+                        text = day,
+                        color = if (MaterialTheme.colors.isLight) disabledColorText else disabledColorTextDark,
+                        textAlign = TextAlign.Center,
+                        fontSize = 13.sp
+                    )
+                }
+
+                items(firstDayOfMonth) {
+                    Box(modifier = Modifier.aspectRatio(1f)) {}
+                }
+                items(dateShown.daysInMonth()) { day ->
+                    val thisDate = LocalDate(dateShown.year, dateShown.monthNumber, day + 1)
+                    val enabled = thisDate in minDate..maxDate
+                    Surface(
+                        modifier = Modifier.aspectRatio(1f),
+                        color = if (thisDate == selectedDate) color else MaterialTheme.colors.surface,
+                        shape = CircleShape
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .aspectRatio(1f)
+                                .clickable(
+                                    enabled = enabled
+                                ) {
+                                    selectedDate =
+                                        LocalDate(dateShown.year, dateShown.monthNumber, day + 1)
+                                },
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = (day + 1).toString(),
+                                style = MaterialTheme.typography.body2,
+                                color = if (enabled) MaterialTheme.colors.onSurface else disabledColorDark
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+        Row(modifier = Modifier.align(alignment = Alignment.End).padding(16.dp)) {
+            TextButton(
+                modifier = Modifier.padding(end = 8.dp),
+                onClick = {
+                    onDismiss()
+                },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = MaterialTheme.colors.error,
+                    backgroundColor = Color.Transparent
+                )
+            ) {
+                Text(cancelText)
+            }
+
+            TextButton(
+                onClick = {
+                    onDateSelected(selectedDate)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = color,
+                    backgroundColor = Color.Transparent
+                )
+            ) {
+                Text(acceptText)
+            }
+        }
+    }
 }
--- library/src/commonMain/kotlin/com/sweetmesoft/kmplibrary/tools/DateUtils.kt
+++ library/src/commonMain/kotlin/com/sweetmesoft/kmplibrary/tools/DateUtils.kt
@@ -1,19 +1,54 @@
 package com.sweetmesoft.kmplibrary.tools
 
+import androidx.compose.runtime.Composable
+import kmp_library.library.generated.resources.Apr
+import kmp_library.library.generated.resources.Aug
+import kmp_library.library.generated.resources.Dec
+import kmp_library.library.generated.resources.Feb
+import kmp_library.library.generated.resources.Fri
+import kmp_library.library.generated.resources.Jan
+import kmp_library.library.generated.resources.Jul
+import kmp_library.library.generated.resources.Jun
+import kmp_library.library.generated.resources.Mar
+import kmp_library.library.generated.resources.May
+import kmp_library.library.generated.resources.Mon
+import kmp_library.library.generated.resources.Nov
+import kmp_library.library.generated.resources.Oct
+import kmp_library.library.generated.resources.Res
+import kmp_library.library.generated.resources.Sat
+import kmp_library.library.generated.resources.Sep
+import kmp_library.library.generated.resources.Sun
+import kmp_library.library.generated.resources.Thu
+import kmp_library.library.generated.resources.Tue
+import kmp_library.library.generated.resources.Wed
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit.Companion.DAY
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
+import kotlinx.datetime.format.DayOfWeekNames
+import kotlinx.datetime.format.MonthNames
 import kotlinx.datetime.format.char
 import kotlinx.datetime.minus
 import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.compose.resources.stringResource
 
 fun getCurrentDateTime(): LocalDateTime {
     return Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+}
+
+fun daysInMonth(year: Int, month: Int): Int {
+    val nextMonth = if (month == 12) {
+        LocalDate(year + 1, 1, 1)
+    } else {
+        LocalDate(year, month + 1, 1)
+    }
+
+    return nextMonth.minus(1, DAY).dayOfMonth
 }
 
 fun Instant.toLocalString(): String {
@@ -40,6 +75,58 @@ fun LocalDate.toLocalString(): String {
     }
 
     return this.format(dateFormat)
+}
+
+@Composable
+fun LocalDate.toLocalString(format: DateFormats = DateFormats.YYYY_MM_DD): String {
+    val weekAbbreviated = listOf(
+        stringResource(Res.string.Mon),
+        stringResource(Res.string.Tue),
+        stringResource(Res.string.Wed),
+        stringResource(Res.string.Thu),
+        stringResource(Res.string.Fri),
+        stringResource(Res.string.Sat),
+        stringResource(Res.string.Sun)
+    )
+    val monthsAbbreviated = listOf(
+        stringResource(Res.string.Jan),
+        stringResource(Res.string.Feb),
+        stringResource(Res.string.Mar),
+        stringResource(Res.string.Apr),
+        stringResource(Res.string.May),
+        stringResource(Res.string.Jun),
+        stringResource(Res.string.Jul),
+        stringResource(Res.string.Aug),
+        stringResource(Res.string.Sep),
+        stringResource(Res.string.Oct),
+        stringResource(Res.string.Nov),
+        stringResource(Res.string.Dec)
+    )
+    val dateFormat = when (format) {
+        DateFormats.YYYY_MM_DD -> LocalDate.Format {
+            date(LocalDate.Formats.ISO)
+        }
+
+        DateFormats.WWW_MMM_DD -> LocalDate.Format {
+            dayOfWeek(DayOfWeekNames(weekAbbreviated))
+            char(',')
+            char(' ')
+            monthName(MonthNames(monthsAbbreviated))
+            char(' ')
+            dayOfMonth()
+        }
+    }
+
+    return this.format(dateFormat)
+}
+
+fun LocalDate.daysInMonth(): Int {
+    val nextMonth = if (this.monthNumber == 12) {
+        LocalDate(year + 1, 1, 1)
+    } else {
+        LocalDate(year, this.monthNumber + 1, 1)
+    }
+    return nextMonth.minus(1, DAY).dayOfMonth
 }
 
 fun LocalTime.toLocalString(showSeconds: Boolean = false): String {
@@ -94,4 +181,15 @@ fun LocalDateTime.minus(
         this.second,
         this.nanosecond
     )
+}
+
+enum class DateFormats(val number: Int) {
+    YYYY_MM_DD(0),
+    WWW_MMM_DD(1);
+
+    companion object {
+        fun fromNumber(number: Int): DateFormats? {
+            return entries.find { it.number == number }
+        }
+    }
 }
