db2fe4f3c26d27a05eedc9a270b11e25b7e4ca31
Erick Steven Velasco Amaya <erickvelasco11@gmail.com>
2026-03-20 16:44:00 -0500

    Updated libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("de.fayard.refreshVersions") version "0.60.6"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "kmplibrary"
